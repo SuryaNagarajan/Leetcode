15d8f6c537b0892c323274423c22f2dd91a2d74a
Surya Nagarajan <43548697+SuryaNagarajan@users.noreply.github.com>
2021-09-25 14:10:51 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (64.16%) - LeetHub

---
+++ subsets/subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  
+vector<vector<int>> subsets(vector<int> nums) {
+    vector<vector<int>> res;
+    int n=nums.size();
+    
+    for(int i=0;i<(1<<n);i++){
+        vector<int> list;
+        for(int j=0;j<n;j++){
+            if(i&(1<<j)){
+                list.push_back(nums[j]);
+            }
+        }
+        res.push_back(list);
+    }
+    
+    return res;
+    }
+};
