71236d0128f1f1ed312908165c39f6a44ab05919
Surya Nagarajan <43548697+SuryaNagarajan@users.noreply.github.com>
2021-10-12 19:01:35 +0530

    Time: 35 ms (13.99%), Space: 43.6 MB (5.59%) - LeetHub

---
+++ champagne-tower/champagne-tower.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> dp(102,vector<double>(102));
+        dp[0][0]=poured*1.0;
+        for(int r=0;r<=query_row;r++){
+            for(int c=0;c<=r;c++){
+                double q=(dp[r][c]-1.0)/2.0;
+                if(q>0){
+                    dp[r+1][c]+=q;
+                    dp[r+1][c+1]+=q;
+                }
+            }
+        }
+        
+        return min(1.0,dp[query_row][query_glass]);
+    }
+};
