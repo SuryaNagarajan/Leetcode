d86931f74086cc9181ef6e9de5aab466c6db5250
Surya Nagarajan <43548697+SuryaNagarajan@users.noreply.github.com>
2021-11-05 08:15:26 +0530

    Time: 4 ms (68.61%), Space: 5.8 MB (96.10%) - LeetHub

---
+++ arranging-coins/arranging-coins.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            n-=i;
+            if(n>=0) count++;
+            else if(n<0) break;
+        }
+        
+        return count;
+    }
+};
